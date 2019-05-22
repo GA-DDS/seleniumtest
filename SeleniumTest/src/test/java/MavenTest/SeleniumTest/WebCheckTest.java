08ba3f34a608f6f9583e9e8c708d3529691d56ad
Murali Kondepati <murali.kondepati@dds.ga.gov>
2019-05-22 11:37:19 -0400

    initial comit

---
+++ SeleniumTest/src/test/java/MavenTest/SeleniumTest/WebCheckTest.java
@@ -0,0 +1,13 @@
+package MavenTest.SeleniumTest;
+
+import org.testng.annotations.Test;
+
+public class WebCheckTest {
+
+	@Test
+
+	public void Login() {
+		System.out.println("Welcome to the WebCheck application");
+		
+	}
+}
